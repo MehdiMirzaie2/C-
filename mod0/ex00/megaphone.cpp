8b547b2103f11118894fedd7bf4cf90432583093
Mehdi Mirzaie <mehdimirzaie18@gmail.com>
2023-08-09 21:36:39 +0930

    finished ex00

--- mod0/ex00/megaphone.cpp
+++ mod0/ex00/megaphone.cpp
@@ -5,27 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mehdimirzaie <mehdimirzaie@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/29 23:21:10 by mehdimirzai       #+#    #+#             */
-/*   Updated: 2023/06/30 09:50:21 by mehdimirzai      ###   ########.fr       */
+/*   Created: 2023/08/08 19:50:34 by mehdimirzai       #+#    #+#             */
+/*   Updated: 2023/08/08 20:03:33 by mehdimirzai      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
-#include <cstring>
 
-int main(int ac, char **av) 
+int	main(int ac, char **av)
 {
-	int i = 1;
-	int len;
-	while (ac > i)
-	{
-		len = strlen(av[i]);
-		for (int k = 0; k < len; k++)
-			putchar(toupper(av[i][k]));
-		std::cout << ' ';
-		i++;
-	}
-	std::cout << '\n';
-	return 0;
+	if (ac == 2)
+		for (int i = 0; i < strlen(av[1]); i++)
+			putchar(toupper(av[1][i]));
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
+	return (0);
 }
