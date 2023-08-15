96c7a9695ee0a612b61b02d9de41be51409acc17
Mehdi Mirzaie <mmirzaie@F3C013.42adel.org.au>
2023-08-15 15:29:52 +0930

    debug

---
+++ mod0/ex01/main.cpp
@@ -0,0 +1,62 @@
+#include "phone.hpp"
+
+int option(std::string input){
+	std::string one		= "ADD";
+	std::string two		= "SEARCH";
+	std::string three	= "EXIT";
+	int length			= input.length();
+
+	if (input.empty())
+		return (0);
+	if (length == 3){
+		for (int i = 0; i < length; i++){
+			if (input[i] != one[i])
+				return (0);
+		}
+		return (1);
+	}
+	else if (length == 6){
+		for (int i = 0; i < length; i++){
+			if (input[i] != two[i])
+				return (0);
+		}
+		return (2);
+	}
+	else if (length == 4){
+		for (int i = 0; i < length; i++){
+			if (input[i] != three[i])
+				return (0);
+		}
+		return (3);
+	}
+	return (0);
+}
+
+int main(void)
+{
+	PhoneBook	book;
+	std::string	input;
+	int			i = 0;
+	int			option_val;
+
+	while (1){
+		do {
+			std::cout << "ADD SEARCH EXIT: ";
+			std::getline(std::cin, input);
+			option_val = option(input);
+		} while (input.length() == 0 || option_val == 0);
+
+		if (option_val == 1){
+			if (i > 7)
+				i = 7;
+			book.ADD(i);
+			i++;
+		}
+		else if (option_val == 2)
+			book.SEARCH(i);
+		else if (option_val == 3)
+			book.EXIT();
+		input.clear();
+	}
+	return (0);
+}
--- mod0/ex01/phone.hpp
+++ mod0/ex01/phone.hpp
@@ -1,31 +1,97 @@
 # ifndef PHONE_HPP
 # define PHONE_HPP
 
-class PhoneBook {       // The class
-  public:             // Access specifier
-    Contact contact[8];   // Attribute (int variable)
-    // string myString;  // Attribute (string variable)
+#include <iostream>
+#include <string>
 
-	int ADD(void){
-		// 
-	}
-
-	int SEARCH(int index){
-		... ;
-	}
-
-	void EXIT(void){
-
-	}
+class Contact {
+	public:
+		std::string firstname;
+		std::string lastname;
+		std::string nickname;
+		std::string darkest_secret;
+		std::string	number;
+		int index;
 };
 
-class Contact {       // The class
-  public:             // Access specifier
-    char first_name[];
-	char last_name[];
-	char nick_name[];
-	int	number;
-	char darkest_secret[];
+class PhoneBook {
+	private:
+		Contact contacts[8];
+
+		void add_spaces(int length){
+			for (int i = length; i < 9; i++)
+				std::cout<<' ';
+		}
+
+	public:
+		void ADD(int i){
+			do {
+				std::cout << "enter firstname: ";
+				std::getline(std::cin, contacts[i].firstname);
+			} while (contacts[i].firstname.length() == 0);
+
+			do {
+				std::cout << "enter lastname: ";
+				std::getline(std::cin, contacts[i].lastname);
+			} while (contacts[i].lastname.length() == 0);
+
+			do {
+				std::cout << "enter nickname: ";
+				std::getline(std::cin, contacts[i].nickname);
+			} while (contacts[i].nickname.length() == 0);
+
+			do {
+				std::cout << "enter number: ";
+				std::getline(std::cin, contacts[i].number);
+			} while (contacts[i].number.length() == 0);
+
+			do {
+				std::cout << "enter darkest secret: ";
+				std::getline(std::cin, contacts[i].darkest_secret);
+			} while (contacts[i].darkest_secret.length() == 0);
+
+			contacts[i].index = i;
+		}
+
+		void SEARCH(int i){
+			int	to_display = 0;
+			int length;
+
+			for (int j = 0; j < i; j++){
+				std::cout<<contacts[j].index<<"         |";
+				if (contacts[j].firstname.length() > 10)
+					std::cout<<contacts[j].firstname.substr(0, 9)<<".|";
+				else{
+					std::cout<<contacts[j].firstname;
+					add_spaces(contacts[j].firstname.length());
+					std::cout<<'|';
+				}
+				if (contacts[j].lastname.length() > 10)
+					std::cout<<contacts[j].lastname.substr(0, 9)<<".|";
+				else{
+					std::cout<<contacts[j].lastname;
+					add_spaces(contacts[j].lastname.length());
+					std::cout<<'|';
+				}
+				if (contacts[j].nickname.length() > 10)
+					std::cout<<contacts[j].nickname.substr(0, 9)<<'.'<<std::endl;
+				else
+					std::cout<<contacts[j].nickname<<std::endl;
+			}
+
+			std::cout<<"which contact would you like: ";
+			std::cin>>to_display;
+
+			std::cout<<"firstname: "<<contacts[to_display].firstname<<std::endl;
+			std::cout<<"lastname: "<<contacts[to_display].lastname<<std::endl;
+			std::cout<<"nickname: "<<contacts[to_display].nickname<<std::endl;
+			std::cout<<"number: "<<contacts[to_display].darkest_secret<<std::endl;
+			std::cout<<"darkest secret: "<<contacts[to_display].number<<std::endl;
+		}
+
+		void EXIT(void){
+			exit(EXIT_SUCCESS);
+		}
 };
 
 #endif
